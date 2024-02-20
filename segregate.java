543cc0ae700f2b2b67299de10fcc3a8e8b54282b
Tarun Ganesh <115348667+tarunganesh2004@users.noreply.github.com>
2024-02-20 10:44:20 +0530

    Create segregate.java

---
+++ segregate.java
@@ -0,0 +1,95 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+public class Main {
+
+        class Node {
+            int data;
+            Node next;
+
+            Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        public Node head = null;
+
+        public void insert(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+            } else {
+                Node temp = head;
+                while (temp.next != null) {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+        }
+        
+        public static void printList(Main list) { 
+        Node currNode = list.head; 
+        while (currNode != null) { 
+            System.out.print(currNode.data + " "); 
+    
+            currNode = currNode.next; 
+        } 
+    } 
+        
+        public  void seggregate(){
+            Node evenStart=null;
+            Node evenEnd=null;
+            Node oddStart=null;
+            Node oddEnd=null;
+            Node curr=head;
+            
+            while(curr!=null){
+                if(curr.data%2==0){
+                    if(evenStart==null){
+                        evenStart=curr;
+                        evenEnd=curr;
+                    } else{
+                        evenEnd.next=curr;
+                        evenEnd=curr;
+                    }
+                }
+                else{
+                    if(oddStart==null){
+                        oddStart=curr;
+                        oddEnd=curr;
+                    } else{
+                        oddEnd.next=curr;
+                        oddEnd=curr;
+                    }
+                }
+                curr=curr.next;
+            }
+            if(evenStart==null){
+                head=oddStart;
+            } else{
+                head=evenStart;
+                evenEnd.next=oddStart;
+                oddEnd.next=null;
+            }
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            Main sll = new Main();
+            for (int i = 0; i < n; i++) {
+                sll.insert(sc.nextInt());
+            }
+            // printList(sll);
+            sll.seggregate();
+            // System.out.println("\n");
+            printList(sll);
+        }
+        
+}
