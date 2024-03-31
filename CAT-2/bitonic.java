3e6ae503e230ddba36057d7b8fb4140f5e4bb42e
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-03-31 17:32:07 +0530

    update bitonic

--- CAT-2/bitonic.java
+++ CAT-2/bitonic.java
@@ -1,12 +1,15 @@
-import
+import java.util.Scanner;
+
 public class bitonic {
     static class node {
         int data;
         node next;
+        node prev;
 
         node(int n) {
             data = n;
             next = null;
+            prev = null;
         }
     }
 
@@ -21,6 +24,7 @@ public class bitonic {
             while (temp.next != null) {
                 temp = temp.next;
             }
+            cur.prev = temp;
             temp.next = cur;
         }
     }
@@ -31,5 +35,58 @@ public class bitonic {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
+    }
+
+    static void bit() {
+        node first = head;
+        node last = head;
+        node res = null;
+        node resend = null;
+        while (last.next != null)
+            last = last.next;
+        // start the condition checking
+        while (first != last) {
+            if (first.data <= last.data) {
+                if (res == null) {
+                    res = resend = first;
+                    first = first.next;
+                } else {
+                    node cur = first.next;
+                    resend.next = first;
+                    first.prev = resend;
+                    cur.prev = null;
+                    first = cur;
+                    resend = resend.next;
+                }
+            } else {
+                if (res == null) {
+                    res = resend = last;
+                    last = last.prev;
+                } else {
+                    node cur = last.prev;
+                    resend.next = last;
+                    last.prev = resend;
+                    cur.next = null;
+                    last = cur;
+                    resend = resend.next;
+                }
+            }
+        }
+        // after while
+        resend.next = first;
+        first.prev = resend;
+        head = res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            insert(sc.nextInt());
+        }
+        display();
+        System.out.println();
+        bit();
+        display();
     }
 }
